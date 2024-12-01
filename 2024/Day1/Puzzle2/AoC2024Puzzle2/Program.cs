2bff2e215d586d9a0490d05ae74d8d2bdd6238f0
Árva Balázs <balazs.arva@windowslive.com>
2024-12-01 21:19:49 +0100

    Day 1 puzzle 2

---
+++ 2024/Day1/Puzzle2/AoC2024Puzzle2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2024Puzzle2;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var input =  await ReadInputAsync();
+
+        var rightListLookup = input.Right
+            .GroupBy(x => x, (key, elements) => new { Number = key, Occurrences = elements.Count() })
+            .ToDictionary(x => x.Number, x => x.Occurrences);
+
+        var result = input.Left
+            .Select(num => rightListLookup.TryGetValue(num, out var occurrences) ? occurrences * num : 0)
+            .Sum();
+
+        Console.WriteLine(result);
+    }
+
+    private static async Task<(IReadOnlyCollection<int> Left, IReadOnlyCollection<int> Right)> ReadInputAsync()
+    {
+        var left = new List<int>(1000);
+        var right = new List<int>(1000);
+
+        using var reader = new StreamReader("Inputs\\input.txt");
+
+        while (!reader.EndOfStream)
+        {
+            var line = await reader.ReadLineAsync();
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                var num1 = int.Parse(Regex.Match(line, "^\\d+").ValueSpan);
+                var num2 = int.Parse(Regex.Match(line, "\\d+$").ValueSpan);
+
+                left.Add(num1);
+                right.Add(num2);
+            }
+        }
+
+        return (left.OrderBy(x => x).ToList(), right.OrderBy(x => x).ToList());
+    }
+}
