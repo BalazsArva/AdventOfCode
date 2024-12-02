39318edccb38d8b133164c2d0f28f1899b7af8f7
BalazsArva <balazs.arva@windowslive.com>
2024-12-02 10:16:53 +0100

    2024 day 2 puzzle 1

---
+++ 2024/Day2/AoCDay2Puzzle1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AoCDay2Puzzle1;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var reports = ReportReader.ReadReportsAsync();
+        var safeReportCount = 0;
+
+        await foreach (var report in reports)
+        {
+            if (report.IsSafe())
+            {
+                ++safeReportCount;
+            }
+        }
+
+        Console.WriteLine(safeReportCount);
+    }
+}
