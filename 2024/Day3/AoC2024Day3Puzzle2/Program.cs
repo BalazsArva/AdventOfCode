78f476a8b3a6b65e00307e4888c9e539149bdf62
BalazsArva <balazs.arva@windowslive.com>
2024-12-03 10:12:10 +0100

    2024 day 3 part 2

---
+++ 2024/Day3/AoC2024Day3Puzzle2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2024Day3Puzzle2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sum = 0;
+        var doOrDont = true;
+        var input = File.ReadAllText("Inputs\\input.txt");
+        var matches = Regex.Matches(
+            input,
+            "(?<mul>mul\\((?<lhs>\\d{1,3}),(?<rhs>\\d{1,3})\\))|(?<do>do\\(\\))|(?<donot>don't\\(\\))",
+            RegexOptions.None,
+            TimeSpan.FromSeconds(10));
+
+        foreach (Match match in matches.Where(x => x.Success))
+        {
+            if (match.Groups["do"].Success)
+            {
+                doOrDont = true;
+            }
+            else if (match.Groups["donot"].Success)
+            {
+                doOrDont = false;
+            }
+            else if (match.Groups["mul"].Success && doOrDont)
+            {
+                var lhs = int.Parse(match.Groups["lhs"].ValueSpan);
+                var rhs = int.Parse(match.Groups["rhs"].ValueSpan);
+
+                sum += lhs * rhs;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
