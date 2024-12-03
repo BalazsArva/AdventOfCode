ddca6b34c93d3587706709d28626848cfacf2c2c
BalazsArva <balazs.arva@windowslive.com>
2024-12-03 10:00:16 +0100

    2024 Day 3 part 1

---
+++ 2024/Day3/AoC2024Day3Puzzle1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2024Day3Puzzle1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllText("Inputs\\input.txt");
+        var matches = Regex.Matches(input, "mul\\((?<lhs>\\d{1,3}),(?<rhs>\\d{1,3})\\)", RegexOptions.None, TimeSpan.FromSeconds(10));
+        var sum = 0;
+
+        foreach (Match match in matches.Where(x => x.Success))
+        {
+            var lhs = int.Parse(match.Groups["lhs"].ValueSpan);
+            var rhs = int.Parse(match.Groups["rhs"].ValueSpan);
+
+            sum += lhs * rhs;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
